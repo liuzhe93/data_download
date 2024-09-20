50c6d6659d0f2aa701237c5d7cc2a5bdd0df77d4
Zhe LIU <liuzhehayida@gmail.com>
2024-09-20 10:47:04 +0800

    Create ViralTrack.sh

---
+++ ViralTrack.sh
@@ -0,0 +1,4 @@
+STAR --runThreadN 5 --runMode genomeGenerate --genomeDir /mnt/zhangzheng_group/liuz-54/data/virus_human_index/index --genomeFastaFiles /mnt/zhangzheng_group/liuz-54/data/virusite/genomes.fasta 
+Rscript Viral_Track_scanning.R Parameters_rmHuman_rmDoubleSlash.txt Target_file.txt
+
+
